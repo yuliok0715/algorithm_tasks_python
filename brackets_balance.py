9081bb2b476cd7146e79b66aef957e21c9494430
yulia <yuliagoncarenko@gmail.com>
2017-11-21 00:57:19 +0200

    added regex for other symbols

--- brackets_balance.py
+++ brackets_balance.py
@@ -6,12 +6,15 @@
 Написать функцию, ктр проверит баланс скобок в заданной строке."""
 import re
 def brackets_balance(input_string):
-    if len(input_string) % 2 != 0:#если скобок нечетное количество - нет пары какой-то скобке
+    if (input_string.count("(") != input_string.count(")"))\
+            or input_string.count("{") != input_string.count("}"):
         return False
     new_string = input_string
-    while re.search("(\(\))+|(\{\})+", new_string) and new_string:#пока в строке есть рядом стоящие скобки и она не пустая
-        new_string = re.sub("(\(\))+|(\{\})+", "", new_string)#удаляем вся рядом стоящие скобки
-    return False if new_string else True#если после этой процедуры получаем пустую строку - все ок
+    while re.search("(\([^\(\)\{\}]*\))+|(\{[^\(\)\{\}]*\})+", new_string) and new_string:#пока в строке есть парные
+        #  скобки и между ними что-либо и она не пустая
+        new_string = re.sub("(\([^\(\)\{\}]*\))+|(\{[^\(\)\{\}]*\})+", "", new_string)#удаляем вся рядом стоящие скобки
+    return False if new_string else True#если после этой процедуры
+    #  получаем пустую строку - все ок
 #test
-print(brackets_balance("({(){}()})()"))
-print(brackets_balance("({(}){})"))
+print(brackets_balance("({(lolik){}ytb(nhy)})()"))
+print(brackets_balance("({(vse ravno}){})"))
