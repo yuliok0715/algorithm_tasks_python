a8048d3061af6c1d8acf8a49e2a8ac3e8633a5fa
yulia <yuliagoncarenko@gmail.com>
2017-12-05 20:31:50 +0200

    new easy task

---
+++ votes.py
@@ -0,0 +1,8 @@
+def votes(groups):
+    groups.sort()
+    return sum(map(lambda x: x // 2 + 1, groups[:len(groups)//2 + 1]))
+
+if __name__ == "__main__":
+    print(votes([2, 5, 6, 4]))
+    print(votes([5, 5, 7]))
+    print(votes([4, 2, 1, 3, 7]))
